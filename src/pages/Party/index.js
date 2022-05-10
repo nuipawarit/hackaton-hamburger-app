6d7840863b97b3fc13a061d5b218743545707b75
firstspector <wachirawut@amity.co>
2022-05-10 20:46:30 +0700

    Dev Party page and party card

--- src/pages/Party/index.js
+++ src/pages/Party/index.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, TextField, Stack, Snackbar } from '@mui/material';
+import PartyCard from './PartyCard'
 
 const Party = () => {
+  const [isJoining, setJoining] = React.useState(false)
+  
+  const handleClick = () => () => {
+    setJoining(true);
+  };
+  
+  const handleClose = () => {
+    setJoining(false);
+  };
+
   return (
     <Box m={2} mt={5}>
       <Typography variant="h4" gutterBottom>
         Odette dinner
       </Typography>
+      <Typography variant="body1" gutterBottom>
+      3 Michellin restaurants Modern French cuisine served amid sophisticated decor in the National Gallery Singapore.
+      </Typography>
+      <TextField fullWidth id="outlined-search" label="Search field" type="search" size='small' sx={{ mx: 'auto', width: '70%' }} />
+      <Stack spacing={2}>
+        <PartyCard title="All on me, I'm rich" name='Markzaa' location='Singapore' trigger={handleClick()}/>
+        <PartyCard title="Not on me , I'm broke" name='Patjung' location='Singapore' trigger={handleClick()}/>
+        <PartyCard title="Daddy's here" name='lnWfirst' location='Singapore' trigger={handleClick()}/>
+      </Stack>
+      <Snackbar
+        anchorOrigin={{ vertical:'top', horizontal:'center' }}
+        open={isJoining}
+        onClose={handleClose}
+        message="Your request is sent Please wait for the host to approve"
+        key={'topcenter'}
+        sx={{ mt: 91, width: '81%', mx: 'auto' }}
+      />
     </Box>
   );
 };
